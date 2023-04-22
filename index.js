575f0e6054d30cdcdde9716ee8ac93772eaf338c
ajaykrishna0079 <120189249+ajaykrishna0079@users.noreply.github.com>
2023-04-22 20:53:24 +0000

    Day01 done

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+
+const { users} = require("./data/users.json");
+const app = express();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+// http://localhost:8081/users/
+http: app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up and running :-)",
+    data: "hey",
+  });
+});
+app.get("/users", (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: users,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.status(404).json({
+    message: "This route doesn't exits",
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
